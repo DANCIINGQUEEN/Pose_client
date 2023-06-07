47e683a96a7c769ab61dee287719c2cd2d78f9b7
park <park>
2023-06-08 00:43:07 +0900

    import md

--- src/prtc/Like.js
+++ src/prtc/Like.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { createRoot } from "https://esm.run/react-dom@18/client";
-import confetti from "https://esm.run/canvas-confetti@1";
+// import confetti from "https://esm.run/canvas-confetti@1";
+import confetti from "canvas-confetti";
 import {Button} from "../UI/UIPackage";
 function Like({text}) {
     function onClick() {
