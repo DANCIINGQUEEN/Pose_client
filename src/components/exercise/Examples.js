add55515df67a94ac7ae61715675629c06a2e707
park <park>
2023-11-14 00:22:58 +0900

    운동 릭크 삽입

--- src/components/exercise/Examples.js
+++ src/components/exercise/Examples.js
@@ -1,10 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import YouTube from 'react-youtube';
 import {Container} from "../UI/UIPackage";
+import youtubeExerciseUrl from "../../config/youtubeExerciseUrl.json";
+import {useLocation} from "react-router-dom";
 
-const VideoPlayer = () => {
+const VideoPlayer = ({url}) => {
     const [isPlaying, setIsPlaying] = useState(false);
-    const [videoTitle, setVideoTitle] = useState('');
+
+    const [playerWidth, setPlayerWidth] = useState(0);
+    const [playerHeight, setPlayerHeight] = useState(0);
+
 
     // 재생 상태 변경 핸들러
     const handlePlayToggle = () => {
@@ -13,50 +18,58 @@ const VideoPlayer = () => {
 
     // YouTube 동영상 옵션 설정
     const opts = {
-        height: '360',
-        width: '640',
+        height: playerHeight,
+        width: playerWidth,
         playerVars: {
             autoplay: 1,
         },
     };
 
-    // 동영상 준비 완료 시 호출되는 콜백 함수
-    const handlePlayerReady = event => {
-        const videoTitle = event.target.getVideoData().title;
-        setVideoTitle(videoTitle);
-    };
+    useEffect(() => {
+        const handleResize = () => {
+            const windowWidth = window.innerWidth;
+            const windowHeight = window.innerHeight;
+            const aspectRatio = 16 / 9; // 가로 세로 비율 (16:9)
+
+            // 브라우저 창의 너비에 따라 비율 계산
+            let width = windowWidth
+            let height = width / aspectRatio;
+
+            // 너비가 화면에 맞지 않을 경우, 높이 기준으로 비율 계산
+            if (height > windowHeight * 0.7) {
+                height = windowHeight * 0.7;
+                width = height * aspectRatio;
+            }
+
+            setPlayerWidth(Math.floor(width));
+            setPlayerHeight(Math.floor(height));
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+
+    const thumbnailSource=`https://img.youtube.com/vi/${url}/maxresdefault.jpg`
+    const thumbnailComponent=<img src={thumbnailSource} alt="Video Thumbnail" onClick={handlePlayToggle}/>
 
     return (
-        <div>
-            {/*{!isPlaying && (*/}
-            {/*    <div>*/}
-            {/*        <h3>{videoTitle}</h3>*/}
-            {/*        <img*/}
-            {/*            src="https://img.youtube.com/vi/nWhS28U6bCY/maxresdefault.jpg"*/}
-            {/*            alt="Video Thumbnail"*/}
-            {/*            onClick={handlePlayToggle}*/}
-            {/*            style={{width:'50%'}}*/}
-            {/*        />*/}
-            {/*    </div>*/}
-            {/*)}*/}
-            {/*{isPlaying && (*/}
-            {/*    <YouTube*/}
-            {/*        videoId="nWhS28U6bCY"*/}
-            {/*        opts={opts}*/}
-            {/*        onReady={handlePlayerReady}*/}
-            {/*        onEnd={handlePlayToggle}*/}
-            {/*    />*/}
-            {/*)}*/}
-        </div>
+        <YouTube videoId={url} opts={opts} onEnd={handlePlayToggle}/>
     );
 };
 
 function Examples(props) {
+    const location = useLocation()
+    const label = location.state?.label || ''
     return (
         <>
-            {/*<VideoPlayer/>*/}
             <br/>
-            <div>hello</div>
+            <VideoPlayer url={youtubeExerciseUrl[label]}/>
+
         </>
     );
 }
